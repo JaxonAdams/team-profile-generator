ea66dd1d11092ab80dd22b867134622221d6dd31
Jaxon Adams <jadamsresume1@gmail.com>
2022-03-15 11:37:33 -0600

    engineer object created and passes tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,15 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates engineer object', () => {
+    const engineer = new Engineer('Jaxon', 54321, 'hey@this.com');
+
+    expect(engineer.name).toBe('Jaxon');
+    expect(engineer.email).toBe('hey@this.com');
+    expect(engineer.id).toBe(54321);
+});
+
+test('tests getGithub()', () => {
+    const engineer = new Engineer('Jaxon', 54321, 'hey@this.com');
+
+    expect(engineer.getGithub('jaxonadams')).toBe('jaxonadams');
+});
